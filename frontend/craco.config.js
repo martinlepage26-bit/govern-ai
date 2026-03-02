9a3c0a6e6ebc575bc90d56567cba6345552a9ea9
martinlepage26-bit <martinlepage26@me.com>
2026-03-02 12:12:01 -0500

    fix: pin ajv and schema-utils, add missing deps

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -5,6 +5,14 @@ module.exports = {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
+    configure: (webpackConfig) => {
+      // Add resolve alias for ajv
+      webpackConfig.resolve.alias = {
+        ...webpackConfig.resolve.alias,
+        'ajv/dist/compile/codegen': path.resolve(__dirname, 'node_modules/ajv/dist/compile/codegen')
+      };
+      return webpackConfig;
+    },
   },
   style: {
     postcss: {
